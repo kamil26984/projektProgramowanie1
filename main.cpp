c55fd4b01492a22bddcf1c2ddc721659d33bcd1a
kamil26984 <s26984@pjwstk.edu.pl>
2022-12-28 01:19:04 +0100

    dodanie plikow z projektu clion

--- main.cpp
+++ main.cpp
@@ -6,6 +6,67 @@
 #include "Word.h"
 #include "WordSet.h"
 using namespace std;
+
+string getUserInput(){
+    string tmp;
+    cin >> tmp;
+    return tmp;
+}
+
+   Word getUserWord() {
+    std::string tmp;
+    std::cout << "podaj slowko i tlumaczenie " << std::endl;
+    Word tmpWord;
+    std::cin >> tmp;
+    tmpWord.setWord(tmp);
+    std::cin >> tmp;
+    tmpWord.setTranslation(tmp);
+    return tmpWord;
+}
+
+WordSet readFile(string file){
+       WordSet newSet;
+       newSet.setName(file);
+       ifstream plik;
+       plik.open("pliki/" + file + ".csv");
+
+       Word w;
+       list<Word> set;
+       newSet.addSet(set);
+       string tmp;
+
+       if(plik.is_open()) {
+           while (getline(plik, tmp)) {
+               stringstream inputString(tmp);
+               getline(inputString, tmp, ';');
+               w.setWord(tmp);
+               getline(inputString, tmp, ';');\
+           w.setTranslation(tmp);
+               newSet.addWord(w);
+           }
+
+           plik.close();
+       }
+       else
+           cout << " nie udalo sie otworzyc pliku\n";
+       return newSet;
+   }
+
+   void printList(list<Word> lista){
+       list<Word>::iterator i;
+       for(i = lista.begin(); i != lista.end(); i++){
+           i->wypisz();
+       }
+   }
+
+
 int main() {
+
+       WordSet testSet;
+       testSet = readFile("zestaw1");
+       printList(testSet.getWords());
+
+
+
     return 0;
 }
